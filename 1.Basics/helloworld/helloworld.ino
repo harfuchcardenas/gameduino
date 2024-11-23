02941bcd4880b38662d9c61c4ef304f0da2f2dad
Agustin Harfuch <agustin.harfuca@gmail.com>
2024-11-23 13:18:58 +0100

    Gameduino repository initialized

---
+++ 1.Basics/helloworld/helloworld.ino
@@ -0,0 +1,20 @@
+#include <GD2.h>
+
+#include <GD2.h>
+
+#include <EEPROM.h>
+#include <SPI.h>
+#include <GD2.h>
+
+void setup()
+{
+  GD.begin(0);
+}
+
+void loop()
+{
+  GD.ClearColorRGB(0x103000);
+  GD.Clear();
+  GD.cmd_text(GD.w / 2, GD.h / 2, 31, OPT_CENTER, "Hello world");
+  GD.swap();
+}
